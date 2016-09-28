7310fc0a9e0b74c9ffdf8fdafa17ae6c562780e9
justintime284 <justintime284@gmail.com>
2016-09-27 22:07:19 -0400

    Updated employee package.

--- src/practice/LCM.java
+++ src/practice/LCM.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  */
 public class LCM {
     public static void main(String[] args) {
-        int[] intArray = {32,450,85};
+        int[] intArray = {32,450,85,5670};
 
         System.out.println(calcLCM(intArray));
     }
@@ -21,7 +21,7 @@ public class LCM {
 
         int lowestCM = intArray[intArray.length - 1];
         boolean found = false;
-        for (int i = 1; i < 100000 && !found; i++){
+        for (int i = 1; !found; i++){
             found = true;
             for (int x : intArray){
                if (lowestCM % x != 0){
